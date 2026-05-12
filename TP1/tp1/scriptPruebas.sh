d3d31bc530d7897c7f985822ff92840dcca48629
hekoh <lucagerace@hotmail.com>
2026-05-12 10:48:03 -0300

    organizo repo

--- TP1/tp1/scriptPruebas.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH -N 1
-#SBATCH --exclusive
-#SBATCH --partition=Blade
-#SBATCH -o /nethome/spusuario9/tp1/output.txt
-#SBATCH -e /nethome/spusuario9/tp1/errors.txt
-#SBATCH --time=00:05:00 #Tiempo límite (HH:MM:SS)
-./Ej2Exe $1 $2
